b2ffe18ec76c60d2f47f165be39241d86ccf7af7
issue87 <issue87@mail.ru>
2024-06-05 23:24:56 +0300

    debug

--- static/js/chess.js
+++ static/js/chess.js
@@ -6,7 +6,7 @@ const figurePositionsOnSourceImage = [
 ];
 const ctx = document.getElementById("gameCanvas").getContext("2d");
 const chessBoardImage = document.createElement("img");
-chessBoardImage.src = "../static/images/chessBoard.jpg";
+chessBoardImage.src = "../images/chessBoard.jpg";
 console.log(ctx);
 console.log(chessBoardImage);
 ctx.drawImage(chessBoardImage, 0, 0);
