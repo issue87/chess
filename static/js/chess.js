f26d604daf6412750c4221432ccd09369def61cb
issue87 <issue87@mail.ru>
2024-06-09 12:01:25 +0300

    debugging

--- static/js/chess.js
+++ static/js/chess.js
@@ -8,7 +8,7 @@ function game(){
   //drawing the chess board
   const canvasWidth = parseInt(canvasEl.offsetWidth);
   const scaleRatio = canvasWidth/chessBoardWidthInPixels;
-  ctx.scale(scaleRatio, scaleRatio); 
+  //ctx.scale(scaleRatio, scaleRatio); 
   ctx.drawImage(chessBoardImage, 160, 360, chessBoardWidthInPixels, chessBoardWidthInPixels, 0, 0, canvasWidth, canvasWidth/2);
 }
 const canvasEl = document.getElementById("gameCanvas");
