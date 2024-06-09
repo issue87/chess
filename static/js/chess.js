1aeb6fd89cf77eda8e414033034d3060d58f6e9a
issue87 <issue87@mail.ru>
2024-06-09 11:54:11 +0300

    debugging

--- static/js/chess.js
+++ static/js/chess.js
@@ -9,7 +9,7 @@ function game(){
   const canvasWidth = parseInt(canvasEl.offsetWidth);
   const scaleRatio = canvasWidth/chessBoardWidthInPixels;
   ctx.scale(scaleRatio, scaleRatio); 
-  ctx.drawImage(chessBoardImage, 160, 360, chessBoardWidthInPixels, chessBoardWidthInPixels, 0, 0, chessBoardWidthInPixels, chessBoardWidthInPixels/2);
+  ctx.drawImage(chessBoardImage, 160, 360, chessBoardWidthInPixels, chessBoardWidthInPixels, 0, 0, canvasWidth, canvasWidth/2);
 }
 const canvasEl = document.getElementById("gameCanvas");
 const ctx = canvasEl.getContext("2d");
