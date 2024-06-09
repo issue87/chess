72416c7d7c9402ce7f15840e6528353e642a47be
issue87 <issue87@mail.ru>
2024-06-09 12:26:14 +0300

    debugging

--- static/js/chess.js
+++ static/js/chess.js
@@ -7,14 +7,15 @@ const figurePositionsOnSourceImage = [
 function game(){
   //drawing the chess board
   const canvasWidth = parseInt(canvasEl.offsetWidth);
-  const scaleRatio = canvasWidth/chessBoardWidthInPixels;
-  //ctx.scale(scaleRatio, scaleRatio);
+  const scaleRatio = canvasWidth/chessBoardWidthInHTML;
+  ctx.scale(scaleRatio, scaleRatio);
   console.log("some");
-  ctx.drawImage(chessBoardImage, 160, 360, chessBoardWidthInPixels, chessBoardWidthInPixels, 0, 0, canvasWidth, canvasWidth/2);
+  ctx.drawImage(chessBoardImage, 160, 360, chessBoardWidthInPixels, chessBoardWidthInPixels, 0, 0, chessBoardWidthInHTML, chessBoardWidthInHTML/2);
 }
 const canvasEl = document.getElementById("gameCanvas");
 const ctx = canvasEl.getContext("2d");
 const chessBoardImage = document.createElement("img");
 chessBoardImage.src = "../static/images/chessBoard.jpg";
 const chessBoardWidthInPixels = 1180;
+const chessBoardWidthInHTML = 1180;
 chessBoardImage.onload = game;
