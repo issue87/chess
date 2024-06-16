cca15d637c32230844f6cd5006266f18f53f4ab1
issue87 <issue87@mail.ru>
2024-06-16 22:25:57 +0300

    debugging

--- static/js/chess.js
+++ static/js/chess.js
@@ -155,10 +155,9 @@ function gameLoad(response){
   for (let i in result_obj){
     const rowPos = result_obj[i].row_pos;
     const colPos = result_obj[i].col_pos;
-    console.log(chessBoard);
     chessBoard[rowPos][colPos] = result_obj[i];
   };
-  for (row in chessBoard){
-    console.log(row);
+  for (let i in chessBoard){
+    console.log(chessBoard[i]);
   }
 };
