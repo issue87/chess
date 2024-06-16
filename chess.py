d5c3ebd54f8c72478cd97581d951e49a04286bba
issue87 <issue87@mail.ru>
2024-06-16 20:05:31 +0300

    debugging

--- chess.py
+++ chess.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, jsonify
 import copy
 import random
 from collections import Counter
