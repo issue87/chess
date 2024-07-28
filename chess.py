cc5eaf9e009ba38e934a872d40e7008b050cce61
issue87 <issue87@mail.ru>
2024-07-28 17:58:47 +0300

    debugging

--- chess.py
+++ chess.py
@@ -356,6 +356,7 @@ class ChessBoard:
         '''
         sets check, if the current player is in check
         '''
+        print(self.king_pos)
         king_pos = self.kings_pos[self.get_current_player().get_color()]
         moves = self.get_possible_moves(self.get_opponent_player().get_color()).values()
         print (king_pos)
